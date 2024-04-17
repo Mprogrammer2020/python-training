5b5e476601a79b2ebc26fe4212549a1c6e6238bc
Ankit Naag <ankit.naag@netsetsoftware.com>
2024-04-17 12:01:00 +0530

    game task

---
+++ rock_paper_scissor.py
@@ -0,0 +1,19 @@
+'''
+Task: Create a simple rock, paper, scissor game
+
+Requirements:
+1. First take user input to choose either 'rock', 'paper' or 'scissor'
+2. Then computer will automatically choose either 'rock', 'paper' or 'scissor'
+3. If user wins, then print "You win", if computer wins, then print "You lose"
+4. If it's a tie, then again take user input and play the game again until user or computer wins
+
+Rules:
+1. rock beats scissor
+2. paper beats rock
+3. scissor beats paper
+
+Test Case:
+1. User can only choose from 'rock', 'paper' or 'scissor'
+'''
+
+# Write your code here
