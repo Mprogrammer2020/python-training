9ca1830a2323fa0c5965fe105c44adf15222d1f0
unknown <abhishek.pandey0512@gmail.com>
2024-04-21 21:18:27 +0530

    update task

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -15,7 +15,32 @@ Rules:
 Test Case:
 1. User can only choose from 'rock', 'paper' or 'scissor'
 '''
+import random
 
-# Write your code here
+possible_actions = ["rock", "paper", "scissors"]
+# validate the user action
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ").lower() 
+    if user_action in possible_actions:
+        break  
+    else:
+        print("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.")
 
-#xyz
+computer_action = random.choice(possible_actions)
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print(" You win! ")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print(" You win!")
+    else:
+        print("You lose.")
