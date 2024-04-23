aba1be3d60cd71cd299847ace7001c67ed1e2c55
Ankit Naag <ankit.naag@netsetsoftware.com>
2024-04-23 09:43:07 +0530

    KBC Task

---
+++ quiz.py
@@ -0,0 +1,13 @@
+'''
+Create a KBC game
+
+- There will be total 5 questions. Each question will have 4 options and one correct answer.
+- If user answer with any wrong option, then the game will be over, Otherwise user will be allowed to move to next question.
+- There will be 2 lifelines available in game.
+    1) 50-50
+    2) Audience Poll
+- If user have used a lifeline, that will be unavailable for next questions.
+- User can exit the game anytime by typing "exit", if they don't want to answer or play.
+- The game will automatically give Rs.10000 for each correct answer, if they give any wrong answer, then they will get nothing.
+- When they exit the game, they will get the total amount they have earned till that point.
+'''
