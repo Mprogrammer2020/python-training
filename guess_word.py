fe4dd111f874ec8090ba8eeac109366ee8943ea8
unknown <abhishek.pandey0512@gmail.com>
2024-04-30 23:33:37 +0530

    update task

--- guess_word.py
+++ guess_word.py
@@ -28,14 +28,14 @@ given_word = "Ab-i--e-"
 
 def guess_word(actual_word, given_word):
     lives = 5
-    guessed_letters = set()  
-    incorrect_guesses = []  
+    guessed_letters = set()
+    incorrect_guesses = []
 
     print("Complete the word by filling the blanks:")
     print(given_word)
 
     while lives > 0 and given_word != actual_word:
-        # Get user input 
+        # Get user input
         user_input = input("Enter a letter: ").lower()
 
         # Validate input
@@ -47,18 +47,21 @@ def guess_word(actual_word, given_word):
             print("You already guessed that letter. Try again.")
             continue
 
-        guessed_letters.add(user_input)  
-
-        if user_input.lower() in actual_word.lower():
+        if user_input in actual_word.lower() and user_input not in given_word.lower():
+            guessed_letters.add(user_input)
             # Update the given word
             for i in range(len(actual_word)):
                 if actual_word[i].lower() == user_input:
                     given_word = given_word[:i] + actual_word[i] + given_word[i + 1:]
             print("You guessed the correct letter:", given_word)
         else:
+            if user_input not in guessed_letters:
+                guessed_letters.add(user_input)
             lives -= 1
-            incorrect_guesses.append(user_input)
+            if user_input not in actual_word.lower():
+                incorrect_guesses.append(user_input)
             print(f"You guessed a wrong letter. {lives} lives left. Incorrect guesses: {', '.join(incorrect_guesses)}")
+
     # all guess wrong
     if lives == 0:
         print("You lost! The word was:", actual_word)
@@ -66,3 +69,4 @@ def guess_word(actual_word, given_word):
         print("You won! You guessed the complete word:", actual_word)
 
 guess_word(actual_word, given_word)
+
