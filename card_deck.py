ffe66a87e1cf2730532900141ec36eccdd86df43
Ankit Naag <ankit.naag@netsetsoftware.com>
2024-05-03 15:37:31 +0530

    count card

---
+++ card_deck.py
@@ -0,0 +1,18 @@
+'''
+There are 52 cards in a deck of cards.
+
+There are 4 persons in the game, i.e Daniel, Bryn, Phil and Justin
+
+You have to distribute these 52 cards to these 4 persons in given way:
+
+Give one card to Daniel, then two cards to Bryn, then three cards to Phil and finally four cards to Justin,
+
+Then again five cards to Daniel, then six cards to Bryn, then seven cards to Phil and finally eight cards to Justin and so on until all the cards are distributed.
+
+Finally display the number of cards each person has received.
+
+Example output:
+Daniel: 4 cards
+Bryn: 10 cards
+and so on.
+'''
