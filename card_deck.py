248aa90b3245a9a3d69102eda248b9e951ace7c0
unknown <abhishek.pandey0512@gmail.com>
2024-05-07 12:54:30 +0530

    Task Update

--- card_deck.py
+++ card_deck.py
@@ -15,56 +15,24 @@ Daniel: 4 cards
 Bryn: 10 cards
 and so on.
 '''
-#  cards each person has
-daniel_cards = 0
-bryn_cards = 0
-phil_cards = 0
-justin_cards = 0
-#cards to give to each person
-daniel_to_give =1
-bryn_to_give = 2
-phil_to_give = 3
-justin_to_give = 4
-
-# total number of cards
+# total deck cards
 total_cards = 52
-# Distribute the cards
-while total_cards > 0:
-    # Give cards to Daniel
-    if total_cards >= daniel_to_give:
-        daniel_cards += daniel_to_give
-        total_cards -= daniel_to_give
-    else:
-        daniel_cards += total_cards
-        total_cards = 0
-    # Give cards to Bryn
-    if total_cards > 0 and total_cards >= bryn_to_give:
-        bryn_cards += bryn_to_give
-        total_cards -= bryn_to_give
-    elif total_cards > 0:
-        bryn_cards += total_cards
-        total_cards = 0
-        
-    # Give cards to Phil
-    if total_cards > 0 and total_cards >= phil_to_give:
-        phil_cards += phil_to_give
-        total_cards -= phil_to_give
-    elif total_cards > 0:
-        phil_cards += total_cards
-        total_cards = 0
+# Players
+players = ["Daniel", "Bryn", "Phil", "Justin"]
 
-    # Give cards to Justin
-    if total_cards > 0 and total_cards >= justin_to_give:
-        justin_cards += justin_to_give
-        total_cards -= justin_to_give
-    elif total_cards > 0:
-        justin_cards += total_cards
-        total_cards = 0
+cards_per_player = [0 for _ in players]
 
-print("Daniel:", daniel_cards)
-print("Bryn:", bryn_cards)
-print("Phil:", phil_cards)
-print("Justin:", justin_cards)
+# Distribute cards
+num_cards = 1
+while sum(cards_per_player) < total_cards:
+  for i in range(len(players)):
+    cards_per_player[i] += num_cards
+    if sum(cards_per_player) >= total_cards:
+      break
+    num_cards += 1
+#player received
+for i in range(len(players)):
+    print(f"{players[i]}: {cards_per_player[i]} cards")
 
          
      
