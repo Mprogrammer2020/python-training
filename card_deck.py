d969834c3aed8ccde4c2f1bde99da597ab04fcf2
unknown <abhishek.pandey0512@gmail.com>
2024-05-08 11:38:23 +0530

    for check

--- card_deck.py
+++ card_deck.py
@@ -15,24 +15,55 @@ Daniel: 4 cards
 Bryn: 10 cards
 and so on.
 '''
-# total deck cards
-total_cards = 52
-# Players
-players = ["Daniel", "Bryn", "Phil", "Justin"]
 
-cards_per_player = [0 for _ in players]
+def distribute_cards(): 
+    daniel_cards = 0
+    bryn_cards = 0
+    phil_cards = 0
+    justin_cards = 0
+    #  card count
+    card_count = 1
 
-# Distribute cards
-num_cards = 1
-while sum(cards_per_player) < total_cards:
-  for i in range(len(players)):
-    cards_per_player[i] += num_cards
-    if sum(cards_per_player) >= total_cards:
-      break
-    num_cards += 1
-#player received
-for i in range(len(players)):
-    print(f"{players[i]}: {cards_per_player[i]} cards")
+    # Distribut cards every person
+    while card_count <= 36:
+        daniel_cards += 1
+        card_count += 1
+        
+        if card_count <= 36:
+            bryn_cards += 2
+            card_count += 2
+            
+        if card_count <= 36:
+            phil_cards += 3
+            card_count += 3
+            
+        if card_count <= 36:
+            justin_cards += 4
+            card_count += 4
+            
+        if card_count <= 36:
+           daniel_cards += 5
+           card_count += 5
+        
+        if card_count <= 36:
+            bryn_cards += 6
+            card_count += 6
+            
+        if card_count <= 36:
+            phil_cards += 7
+            card_count += 7
+            
+        if card_count <= 36:
+            justin_cards += 8
+            card_count += 8             
+    #add remaining cards to Daniel
+    daniel_cards += 16
+   
+    print("Daniel:", daniel_cards)
+    print("Bryn:", bryn_cards)
+    print("Phil:", phil_cards)
+    print("Justin:", justin_cards)
+distribute_cards()
 
          
      
