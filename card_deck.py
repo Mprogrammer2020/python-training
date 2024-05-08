8c20fc3246f02505b04aa27c62f1f9885fb4f4a3
unknown <abhishek.pandey0512@gmail.com>
2024-05-08 16:36:25 +0530

    task update

--- card_deck.py
+++ card_deck.py
@@ -1,4 +1,4 @@
-''''
+'''But I've one issue 
 There are 52 cards in a deck of cards.
 
 There are 4 persons in the game, i.e Daniel, Bryn, Phil and Justin
@@ -16,56 +16,28 @@ Bryn: 10 cards
 and so on.
 '''
 
-def distribute_cards(): 
-    daniel_cards = 0
-    bryn_cards = 0
-    phil_cards = 0
-    justin_cards = 0
-    #  card count
-    card_count = 1
 
-    # Distribut cards every person
-    while card_count <= 36:
-        daniel_cards += 1
-        card_count += 1
-        
-        if card_count <= 36:
-            bryn_cards += 2
-            card_count += 2
-            
-        if card_count <= 36:
-            phil_cards += 3
-            card_count += 3
-            
-        if card_count <= 36:
-            justin_cards += 4
-            card_count += 4
-            
-        if card_count <= 36:
-           daniel_cards += 5
-           card_count += 5
-        
-        if card_count <= 36:
-            bryn_cards += 6
-            card_count += 6
-            
-        if card_count <= 36:
-            phil_cards += 7
-            card_count += 7
-            
-        if card_count <= 36:
-            justin_cards += 8
-            card_count += 8             
-    #add remaining cards to Daniel
-    daniel_cards += 16
-   
-    print("Daniel:", daniel_cards)
-    print("Bryn:", bryn_cards)
-    print("Phil:", phil_cards)
-    print("Justin:", justin_cards)
-distribute_cards()
+total_cards = 52
+players = ["Daniel", "Bryn", "Phil", "Justin"]
 
-         
-     
-                              
-    
+cards_per_player = [0 for _ in players]
+
+# Distribute cards
+def distribute_cards(total_cards, cards_per_player):
+    num_cards = 1
+    while total_cards > 0:
+        for i in range(len(players)):
+            if total_cards >= num_cards:
+                cards_per_player[i] += num_cards
+                total_cards -= num_cards
+            else:
+                  # give the remaining cards
+                cards_per_player[i] += total_cards
+                total_cards = 0
+                break
+            num_cards += 1 
+    return cards_per_player
+cards_per_player = distribute_cards(total_cards, cards_per_player)
+
+for i in range(len(players)):
+    print(f"{players[i]} received {cards_per_player[i]} cards.")
