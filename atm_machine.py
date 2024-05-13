4e8f695777f745223549b06d90b22c28ff2f06bb
Ankit Naag <ankit.naag@netsetsoftware.com>
2024-05-13 12:56:17 +0530

    atm machine task

---
+++ atm_machine.py
@@ -0,0 +1,12 @@
+'''
+Task: Create a simple ATM machine program, where user can:
+1) Create an account (Open user account and set initial mininum balance to Rs.1000)
+2) Deposit Money
+3) Withdraw Money
+4) Check Balance
+
+Test Case:
+1. User can only choose from '1', '2', '3' or '4'
+2. User need to enter their account number for deposit, withdraw and check balance
+3. User is required to have Rs.1000 minimum balance in their account
+'''
