82ec04ef930a340ba47f61a554ec2a17f922b151
unknown <abhishek.pandey0512@gmail.com>
2024-07-16 14:37:56 +0530

    CRUD_REST_API

--- PythonDjango/crud_pro/emp/api/serializers.py
+++ PythonDjango/crud_pro/emp/api/serializers.py
@@ -5,7 +5,7 @@ from emp.models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'date_of_birth')
+        fields = ('id', 'username', 'email', 'date_of_birth','password')
 
         
 class LoginSerializer(serializers.Serializer):
